702c6af166daf306598b51ead53d7ae97ce65c53
Maxim COMPS <maxim.comps@gmail.com>
2026-02-19 13:01:07 +0100

    content maj

--- JS/anims.js
+++ JS/anims.js
@@ -1,11 +1,13 @@
 const pjslides = [
     { img: "Images/Projets/choppe-lapin_cover.jpg", title: "CHOPPE-LAPIN [IE S3]", text: "C’est un jeu interactif utilisant un écran et un télémètre laser où le joueur doit attraper un lapin en ajustant la vitesse de sa main selon son état, le projet ayant consisté à améliorer la précision des mesures du capteur et à optimiser l’affichage pour que l’expérience reste fluide et réactive.", link: "projet-choppelapin.html" },
+    { img: "Images/Projets/chaine-de-production_cover.jpg", title: "CHAINE DE PRODUCTION [AUTOM S2]", text: "C’est un projet de chaîne de production automatisée sur Control Expert où chaque pièce est acheminée et triée selon son type, la station 4 assurant le tri précis vers les zones dédiées, avec des améliorations comme le comptage des pièces et la gestion des dépôts pour garantir un fonctionnement fluide et sans erreur.", link: "projet-chainedeproduction.html" },
     { img: "Images/Projets/smart-car_cover.jpg", title: "SMART-CAR [IE/ELEC S2]", text: "Voici un autre contenu avec une image différente.", link: "projet-smartcar.html" },
-    { img: "Images/Projets/chaine-de-production_cover.jpg", title: "CHAINE DE PRODUCTION [AUTOM S2]", text: "Encore une troisième description.", link: "projet-chainedeproduction.html" },
-    { img: "Images/Projets/electrocardiographe_cover.jpg", title: "ELECTROCARDIOGRAPHE [ELEC S3]", text: "Encore une troisième description.", link: "projet-electrocardiographe.html" },
+    { img: "Images/Projets/translateur-rotatif_cover.jpg", title: "TRANSLATEUR ROTATIF [AUTOM S3]", text: "Encore une troisième description.", link: "projet-translateurrotatif.html" },
+    { img: "Images/Projets/station-de-pompage_cover.jpg", title: "STATION DE POMPAGE [AUTOM S3]", text: "Encore une troisième description.", link: "projet-stationdepompage.html" },
     { img: "Images/Projets/robot-mobile_cover.jpg", title: "ROBOT MOBILE [IE/ELEC S2]", text: "Encore une troisième description.", link: "projet-robotmobile.html" },
+    { img: "Images/Projets/electrocardiographe_cover.jpg", title: "ELECTROCARDIOGRAPHE [ELEC S3]", text: "Encore une troisième description.", link: "projet-electrocardiographe.html" },
+    { img: "Images/Projets/bras-manipulateur_cover.jpg", title: "BRAS MANIPULATEUR [ROBO S4]", text: "Encore une troisième description.", link: "projet-brasmanipulateur.html" },
     { img: "Images/Projets/volet-roulant_cover.jpg", title: "VOLET ROULANT [AUTOM S1]", text: "Encore une troisième description.", link: "projet-voletroulant.html" },
-    { img: "Images/Projets/bras-manipulateur_cover.jpg", title: "BRAS MANIPULATEUR [ROBO S4]", text: "Encore une troisième description.", link: "projet-brasmanipulateur.html" }
 ];
 
 const expslides = [
